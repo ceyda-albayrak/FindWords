390c125ec05082669e53f985683fa835f5a79833
ceyda-albayrak <ceydaa2098@gmail.com>
2021-03-02 23:45:56 +0300

    feat: add find words project.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace kelimebulmaca
+{
+    partial class frmbul
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnbasla = new System.Windows.Forms.Button();
+            this.lstbxkelimeler = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnbasla
+            // 
+            this.btnbasla.Location = new System.Drawing.Point(21, 12);
+            this.btnbasla.Name = "btnbasla";
+            this.btnbasla.Size = new System.Drawing.Size(115, 23);
+            this.btnbasla.TabIndex = 0;
+            this.btnbasla.Text = "Oyuna Başla";
+            this.btnbasla.UseVisualStyleBackColor = true;
+            this.btnbasla.Click += new System.EventHandler(this.btnbasla_Click);
+            // 
+            // lstbxkelimeler
+            // 
+            this.lstbxkelimeler.FormattingEnabled = true;
+            this.lstbxkelimeler.Location = new System.Drawing.Point(21, 76);
+            this.lstbxkelimeler.Name = "lstbxkelimeler";
+            this.lstbxkelimeler.Size = new System.Drawing.Size(125, 342);
+            this.lstbxkelimeler.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kelimeler";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(174, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(364, 339);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(316, 436);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // frmbul
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 462);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstbxkelimeler);
+            this.Controls.Add(this.btnbasla);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frmbul";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kelime Bulmaca - www.bilisimogretmeni.com";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnbasla;
+        private System.Windows.Forms.ListBox lstbxkelimeler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace kelimebulmaca
+{
+    public partial class frmbul : Form
+    {
+        public frmbul()
+        {
+            InitializeComponent();
+        }
+        string dosyayol, satir, kelime = "", butonname;
+        List<string> lstkelimler = new List<string>();
+        List<int> lstindexno = new List<int>();
+        List<Control> butonlar = new List<Control>();
+        List<Control> pasifbutonlar = new List<Control>();
+        public List<int> sayiuret(int kelimezunluk)
+        {//kelimenin haflerini karıştırmak için rastgele tekrarsız sayılar üretilir
+            //http://www.bilisimogretmeni.com/
+            List<int> rastgelesayilar = new List<int>();
+            Random rnd = new Random();
+            int sayi, sayac = 0;
+            do
+            {
+                sayi = rnd.Next(0, kelimezunluk);
+                if (rastgelesayilar.IndexOf(sayi) == -1)
+                {
+                    rastgelesayilar.Add(sayi);
+                    sayac++;
+                }
+            } while (sayac != kelimezunluk);
+
+            return rastgelesayilar;
+        }
+        public List<string> kelimeyaz()
+        {//text doyası içinde yer alan kelimeler okunup listeye eklenir
+            //http://www.bilisimogretmeni.com/
+            List<string> yedekkelimler = new List<string>();
+            dosyayol = Directory.GetCurrentDirectory() + "\\kelimeler.txt";
+            FileStream dosyaerisim = new FileStream(dosyayol, FileMode.Open, FileAccess.Read);
+            StreamReader okuyucu = new StreamReader(dosyaerisim);
+            satir = okuyucu.ReadLine();
+            do
+            {
+                yedekkelimler.Add(satir.ToUpper());
+                satir = okuyucu.ReadLine();
+                if (satir == null)
+                {
+                    break;
+                }
+
+            } while (satir != null);
+
+            foreach (string item in yedekkelimler)
+            {
+                lstbxkelimeler.Items.Add(item);
+            }
+            return yedekkelimler;
+            //kelimler list olarak return edilir
+            //http://www.bilisimogretmeni.com/
+        }
+
+        private void btnbasla_Click(object sender, EventArgs e)
+        {
+            lstbxkelimeler.Items.Clear();
+            groupBox1.Controls.Clear();
+            label2.Text = "";
+            int harfno = 0;
+            List<char> lstharfler = new List<char>();
+            List<int> lstharfindexno = new List<int>();
+            lstkelimler = kelimeyaz();
+
+
+            for (int i = 0; i < lstkelimler.Count; i++)
+            {//kelimler sırası ile alınarak harfleri rastgele karıştırılıp listeye eklenir
+                //http://www.bilisimogretmeni.com/
+                lstindexno.Clear();
+                lstindexno = sayiuret(lstkelimler[i].Length);
+
+                foreach (int item in lstindexno)
+                {
+                    lstharfler.Add(lstkelimler[i][item]);
+                    //listBox1.Items.Add(lstkelimler [i][ item].ToString ());
+                }
+            }
+            lstharfindexno = sayiuret(90);
+            //kelimelerdeki toplam harf sayısı 90 olduğu için 90 adet rastgele sayı üretilir
+            //http://www.bilisimogretmeni.com/
+            //bu sayılar hafler listesinden rastgele harf seçmek için kullanılır.
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Button btnharf = new Button();
+                    btnharf.Height = 25;
+                    btnharf.Width = 25;
+                    btnharf.Text = lstharfler[lstharfindexno[harfno]].ToString();
+                    harfno++;
+                    btnharf.BackColor = Color.White;
+                    btnharf.Location = new Point(10 + (j * 35), 10 + (i * 35));
+                    btnharf.Name = (10 + (j * 35)).ToString() + (10 + (i * 35)).ToString();
+                    btnharf.Click += new EventHandler(btnharf_click);
+                    groupBox1.Controls.Add(btnharf);
+                }
+            }
+
+        }
+
+        public void btnharf_click(object sender, System.EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Enabled = false;
+            kelime += btn.Text;
+            if (kelime.Length > 7)
+            {//her tıklanan butonun texi yan yana eklenerek kelimeler listesinde olup olmadığı kontrol edilir.
+                //listede yer alan kelimlerden en uzun kelime 7 harfli olduğu için 7 rakamı kullanıldı.
+                kelime = "";
+                kelime += btn.Text;
+            }
+
+            label2.Text = kelime;
+            butonname = btn.Location.X.ToString() + btn.Location.Y.ToString();
+            if (butonname == btn.Name)
+            {
+                butonlar.Add(btn);
+                pasifbutonlar.Add(btn);
+            }
+
+            if (lstkelimler.IndexOf(kelime) > -1)
+            {//butonlar tıklanarak bulunan kelime listede var ise listeden silinir
+                //http://www.bilisimogretmeni.com/
+                lstkelimler.Remove(kelime);
+                kelime = "";
+
+                lstbxkelimeler.Items.Clear();
+                for (int i = 0; i < butonlar.Count; i++)
+                {//doğru kelimeyi oluşturan harflerin yer aldığı butonlar pasif ve renkli yapılabilmek için bir liste 
+                    //saklanır
+                    butonlar[i].Enabled = false;
+                    butonlar[i].BackColor = Color.Red;
+                }
+                butonlar.Clear();
+                pasifbutonlar.Clear();
+                foreach (string item in lstkelimler)
+                {
+                    lstbxkelimeler.Items.Add(item);
+                }
+            }
+            else if (kelime.Length > 6)
+            {
+                //eğer arka arkaya tıklanan 7 buton sonuda ortaya çıkan kelime kelime listesinde yok ise pasifleştirilen butonlar
+                //tekrar aktif hale getirilir
+                for (int i = 0; i < pasifbutonlar.Count; i++)
+                {
+                    pasifbutonlar[i].Enabled = true;
+                }
+                pasifbutonlar.Clear();
+                butonlar.Clear();
+            }
+
+
+        }
+    }
+}
